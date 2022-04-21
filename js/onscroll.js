4113694dc36b3f36d6decc6d606f5b4c41fb67df
Clayton Barnett <Cbarnett427@live.com>
2022-04-21 15:32:02 -0500

    Update v0.5.8

--- js/onscroll.js
+++ js/onscroll.js
@@ -2,11 +2,11 @@
 window.onscroll = function() {scrollFunction()};
 // boxShadow text as string into a variable
 var classattrib = "box-shadowinset 1px 3px 5px 3px rgba(12,12,12,20);";
-var nodeList = document.getElementsByClassName('nav-link')
+// var nodeList = document.getElementsByClassName('nav-link')
 // document.getElementById("navbar").style.cssText = classattrib;
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.backgroundColor = "#ffffff";
     document.getElementById("navbar").style.boxShadow = "1px 3px 5px 3px #0c0c0c33";
     // document.getElementById("logo").style.color = "#ffffff";
@@ -17,10 +17,10 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.boxShadow = "none";
-    document.getElementById("logo").style.color = "#363636";
-    var els = document.getElementsByClassName('nav-link');
-    for (var i = 0; i < els.length; i++) {
-      els[i].style.color = '#363636';
-    }
+    // document.getElementById("logo").style.color = "#363636";
+    // var els = document.getElementsByClassName('nav-link');
+    // for (var i = 0; i < els.length; i++) {
+    //   els[i].style.color = '#363636';
+    // }
   }
 }
