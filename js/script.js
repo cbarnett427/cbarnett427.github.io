39582568161b0eb8b806585e3cc687238566df39
Clayton Barnett <Cbarnett427@live.com>
2022-05-10 10:58:22 -0500

    Update v0.6.2
    Added Improved Dark Mode

--- js/script.js
+++ js/script.js
@@ -40,3 +40,29 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('color-mode', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('color-mode', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('color-mode', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
